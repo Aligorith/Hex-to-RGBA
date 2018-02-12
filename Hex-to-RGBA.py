a3e8785b96c4afa2b7699e2afe7572e3f32b4378
Joshua Leung <aligorith@gmail.com>
2018-02-12 19:07:38 +1300

    Patched version of the Hex-to-RGBA command so that it works in ST3 (Build 3143)
    Fixes:
    1) Regex expression was broken:
       i. '#' was unescaped, causing entire expression to be treated as a comment
       ii. It assumed that text would appear at end of the line (with no semi-colon)
    
    2) self.view.word() now includes the whitespace preceeding the word,
       so this needs to be stripped (and accounted for) or else the lookups
       would fail
    
    3) Convert strings to lowercase so that upper/lowercase is not a problem
    
    
    Notes:
    * Debug prints have been left in here for the commit-history benefits

--- Hex-to-RGBA.py
+++ Hex-to-RGBA.py
@@ -5,29 +5,46 @@ import sublime_plugin
 
 class HexToRgbaCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        print("Running hex2rgba")
         for selection in self.view.sel():
+            print("  sel...")
             word_region = self.view.word(selection)
+            print("word_region = %s" % (word_region))
             if not word_region.empty():
-                rgba_css = self.convert_to_rgba_css(word_region)
+                word = self.view.substr(word_region)
+                print("word = '%s'" % (word))
+                start_idx = word_region.begin()
+                while (len(word) > 0) and (word[0].isspace()):
+                    word = word[1:]
+                    start_idx += 1
+                print("stripped word = '%s' (at %d)" % (word, start_idx))
+
+                rgba_css = self.convert_to_rgba_css(word)
                 if rgba_css:
-                    if (self.view.substr(word_region.begin()-1) == "#"):
-                        tmp_region = sublime.Region(word_region.begin()-1, word_region.end())
+                    print("rgba_css = '%s'" % (rgba_css))
+                    if (word[0] != '#'):
+                        tmp_region = sublime.Region(start_idx-1, word_region.end())
                         self.view.replace(edit, tmp_region, rgba_css)
-                    elif (self.view.substr(word_region.begin()) == "#"):
-                        tmp_region = sublime.Region(word_region.begin(), word_region.end())
+                    else:
+                        tmp_region = sublime.Region(start_idx, word_region.end())
                         self.view.replace(edit, tmp_region, rgba_css)
+                else:
+                    print("no rgba_css - '%s'" % (rgba_css))
 
     def hex_to_rgba(self, value):
-        value = value.lstrip('#')
+        value = value.lower().lstrip('#')
         if len(value) == 3:
             value = ''.join([v*2 for v in list(value)])
         return tuple(int(value[i:i+2], 16) for i in range(0, 6, 2))+(1,)
 
-    def convert_to_rgba_css(self, word_region):
-        word = self.view.substr(word_region)
-        re_hex_color = re.compile('#?([0-9a-fA-F]{3}([0-9a-fA-F]{3})?){1}$')
-        if re_hex_color.match(word):
+    def convert_to_rgba_css(self, word):
+        print("word substr = '%s'" % (word))
+        re_hex_color = re.compile('\#?([0-9a-fA-F]{3}([0-9a-fA-F]{3})?){1}')
+        match_result = re_hex_color.match(word)
+        print("--> match result = %s" % (match_result))
+        if match_result:
             rgba = self.hex_to_rgba(word)
             rgba_css = 'rgba(%s,%s,%s,%s)' % rgba
             return rgba_css
+        print("--> failed to match - %s" % (re_hex_color))
         return False
